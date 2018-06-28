3b410f473074e7b6ddcccd1f3ffb7c87633eaf87
Vaclav Blazek <vaclav.blazek@citationtech.net>
2018-06-28 11:48:37 +0200

    fixed atmdensity devilery

--- vtsd/src/vtsd/delivery/vts/driver.cpp
+++ vtsd/src/vtsd/delivery/vts/driver.cpp
@@ -638,6 +638,12 @@ void VtsStorageView::handle(Sink sink, const Location &location
         return sink.content(info.support->second);
     }
 
+    if (info.service) {
+        return sink.content(vts::service::generate
+                            (info.service, info.path, location.query)
+                            , FileClass::data);
+    }
+
     // wtf?
     sink.error(utility::makeError<NotFound>("Unknown file."));
 }
@@ -764,12 +770,6 @@ void VtsTileIndex::handle(Sink sink, const Location &location
     if (info.support) {
         // support file
         return sink.content(info.support->second);
-    }
-
-    if (info.service) {
-        return sink.content(vts::service::generate
-                            (info.service, info.path, location.query)
-                            , FileClass::data);
     }
 
     // wtf?
